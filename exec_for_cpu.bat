2a2df1483d35939671d356b0aa182758151cb1c4
WON <teadone@naver.com>
2020-04-07 00:48:40 +0900

    rollback

--- exec_for_cpu.bat
+++ exec_for_cpu.bat
@@ -1,4 +1,3 @@
-powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5','./mask_rcnn_coco.h5')"&call activate.bat
 call pip install -r requirements.txt
 
 cd code
