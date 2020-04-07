3694ba7a3dd1e08a226206251fccab5b9b156dac
WON <teadone@naver.com>
2020-04-07 15:15:03 +0900

    modify the file download cause error.

--- exec_for_gpu.bat
+++ exec_for_gpu.bat
@@ -1,5 +1,4 @@
-start powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5','./mask_rcnn_coco.h5')"
-call activate.bat
+powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5','./mask_rcnn_coco.h5')"&call activate.bat
 call conda_pack.bat
 call pip_pack.bat
 
