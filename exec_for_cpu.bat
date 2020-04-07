cdd6e12c06fea0a941374b8822df5e1ded61090b
WON <teadone@naver.com>
2020-04-07 12:26:32 +0900

    modify the detection to none

--- exec_for_cpu.bat
+++ exec_for_cpu.bat
@@ -1,5 +1,5 @@
 start powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5','./mask_rcnn_coco.h5')"
-call activate.bat detection
+call activate.bat
 call pip install -r requirements.txt
 
 cd code
