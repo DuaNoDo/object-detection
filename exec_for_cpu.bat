c47d12419602a6fe7d6e279130b7bdd4fd081ea5
WON <teadone@naver.com>
2020-04-07 12:05:29 +0900

    add powershell , so it download h5 file automatic

--- exec_for_cpu.bat
+++ exec_for_cpu.bat
@@ -1,3 +1,4 @@
+start powershell -Command "(new-object System.Net.WebClient).DownloadFile('https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5','./mask_rcnn_coco.h5')"
 call pip install -r requirements.txt
 
 cd code
