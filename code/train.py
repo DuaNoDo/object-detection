7f24612519cfe6e46669fdab49db774bd7e3ef73
Your Name <you@example.com>
2021-05-27 14:34:16 +0900

    via error solve

--- code/train.py
+++ code/train.py
@@ -121,7 +121,18 @@ class objectDataset(utils.Dataset):
         # We mostly care about the x and y coordinates of each region
         # Note: In VIA 2.0, regions was changed from a dict to a list.
         annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json"), encoding="utf-8"))
-        annotations = list(annotations.values())  # don't need the dict keys
+        via_1_check = annotations.get('regions')
+        via_2_check = annotations.get('_via_img_metadata')
+
+        # JSON is formatted with VIA-1.x
+        if via_1_check:
+            annotations = list(annotations.values())
+        # JSON is formatted with VIA-2.x
+        elif via_2_check:
+            annotations = list(annotations['_via_img_metadata'].values())
+        # Unknown JSON formatting
+        else:
+            raise ValueError('The JSON provided is not in a recognised via-1.x or via-2.x format.')
 
         # The VIA tool saves images in the JSON even if they don't have any
         # annotations. Skip unannotated images.
